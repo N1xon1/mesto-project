fb22c7c4bbdd62f99d19af469ce55fce671390aa
N1xon1 <nikitakemov.2005@gmail.com>
2025-01-30 13:04:32 +0300

    создания шаблона проекта

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// @todo: Темплейт карточки
+const template = document.querySelector("#card-template");
+const image = document.querySelector(".card__image");
+const title = document.querySelector(".card__title");
+const item = template.content.cloneNode(true);
+
+// @todo: DOM узлы
+
+// @todo: Функция создания карточки
+
+// @todo: Функция удаления карточки
+
+// @todo: Вывести карточки на страницу
