8befa5b546f9f0344a8f0f9275697a270c8f35be
N1xon1 <nikitakemov.2005@gmail.com>
2025-02-01 06:29:56 +0300

    Добавлена функционал добавление карточки пользователем

--- scripts/index.js
+++ scripts/index.js
@@ -17,19 +17,41 @@ const descripProfile = document.querySelector(".profile__description");
 
 const profileFormElement = profilePopup.querySelector(".popup__form");
 const btnProfileSave = profilePopup.querySelector(".popup__button");
+// Открытие и закрытие формы для добавления карточек
+const btnCardOpen = document.querySelector(".profile__add-button");
+const btnCardClose = cardPopup.querySelector(".popup__close");
+const btnCardSave = cardPopup.querySelector(".popup__button");
+btnCardOpen.addEventListener("click", () => openModal(cardPopup));
+btnCardClose.addEventListener("click", () => closeModal(cardPopup));
+// Добавления новых карточек пользователем
+const CardSave = cardPopup.querySelector(".popup__form");
+const cardName = cardPopup.querySelector(".popup__input_type_card-name");
+const cardUrl = cardPopup.querySelector(".popup__input_type_url");
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+  createCard(cardName.value, cardUrl.value, true);
+  CardSave.reset();
+  closeModal(cardPopup);
+}
+CardSave.addEventListener("submit", handleCardFormSubmit);
+
 // Перебор массива с данными
 initialCards.forEach(function (elem) {
-  createCard(elem.name, elem.link);
+  createCard(elem.name, elem.link, false);
 });
 // Функция создания карточки
-function createCard(name, link) {
+function createCard(name, link, isUserCard) {
   const item = template.content.cloneNode(true);
   const img = item.querySelector(".card__image");
   const title = item.querySelector(".card__title");
   img.setAttribute("src", link);
   img.setAttribute("alt", `Фото ${name}`);
   title.textContent = name;
-  return appendCard.append(item);
+  if (isUserCard) {
+    return appendCard.prepend(item);
+  } else {
+    return appendCard.append(item);
+  }
 }
 
 // @todo: Функция удаления карточки
@@ -38,29 +60,18 @@ function removeCard() {}
 titleProfilePopup.value = titleProfile.textContent;
 descripProfilePopup.value = descripProfile.textContent;
 // Обработчик события открытия окна редактирования профиля
-btnProfileOpen.addEventListener("click", (event) => {
-  openModal(profilePopup);
-});
+btnProfileOpen.addEventListener("click", () => openModal(profilePopup));
 // Сохранение изменений профиля
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   titleProfile.textContent = titleProfilePopup.value;
   descripProfile.textContent = descripProfilePopup.value;
+  closeModal(profilePopup);
 }
 profileFormElement.addEventListener("submit", handleProfileFormSubmit);
-// Обработчики события закрытия формы после сохранения
-btnProfileSave.addEventListener("click", (event) => {
-  closeModal(profilePopup);
-});
-btnProfileSave.addEventListener("key", (event) => {
-  if (event.key === "Enter") {
-    closeModal(profilePopup);
-  }
-});
+
 // Обработчик события закрытия окна редактирования профиля
-btnProfileClose.addEventListener("click", (event) => {
-  closeModal(profilePopup);
-});
+btnProfileClose.addEventListener("click", () => closeModal(profilePopup));
 
 // Функция окрытия окна
 function openModal(popup) {
